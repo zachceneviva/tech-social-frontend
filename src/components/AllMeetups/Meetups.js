28553d3ec612067d7fc16ca045c5ed4f4d6363db
zach-ceneviva <zceneviva@gmail.com>
2021-11-30 21:25:47 -0600

    added all meetups page

--- src/components/AllMeetups/Meetups.js
+++ src/components/AllMeetups/Meetups.js
@@ -1,20 +1,19 @@
 import styles from "./Meetups.module.scss"
 import { Button } from "react-bootstrap"
+import { format } from "date-fns"
 
-export default function Meetups () {
+export default function Meetups (props) {
     return (
-        <div className={styles.allGroups}>
             <div className={styles.allGroupsCard}>
-                <img src="https://iupac.org/wp-content/uploads/2018/05/default-avatar.png" alt="user" />
+                <img src={props.meetup.photo} alt="user" />
                 <div className={styles.allGroupsContent}>
                     <div className={styles.groupRole}>
-                        <h4>Code and Coffee</h4>
-                        <h6>Nashville, TN</h6>
-                        <p>6/23/2021</p>
+                        <h4>{props.meetup.name}</h4>
+                        <h6>{!props.meetup.city && !props.meetup.state ? props.meetup.address : `${props.meetup.city}, ${props.meetup.state}`}</h6>
+                        <p>{format(new Date(props.meetup.date), 'MM/dd/yyyy')}</p>
                     </div>
-                <Button>View</Button>
+                <Button className={styles.viewBtn} href={`/meetup/${props.meetup._id}`}>View</Button>
                 </div>
             </div>
-        </div>
     )
 }
--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,8 +1,23 @@
+import React, {useState, useEffect} from "react"
 import styles from "./AllMeetups.module.scss"
 import BannerProfileCard from "../components/Feed/BannerProfileCard"
 import Meetups from "../components/AllMeetups/Meetups"
+import axios from "axios"
 
 export default function AllMeetups () {
+    const [allMeetups, setAllMeetups] = useState([])
+    const [busy, setBusy] = useState(true)
+
+    useEffect(() => {
+        console.log("fetching...")
+        axios.get(`http://localhost:4000/api/v1/techonnect/meetups`).then((res) => res.data).then(res => setAllMeetups(res.meetups))
+        setBusy(false)
+    }, [busy])
+    
+
+    const meetup = allMeetups.map((meetup, idx) => {
+        return <Meetups meetup={meetup} key={idx}/>
+    })
     return (
         <div className={styles.mainContainer}>
             <div className={styles.mainContentContainer}>
@@ -10,7 +25,9 @@ export default function AllMeetups () {
                     <BannerProfileCard/>
                 </div>
                 <div className={styles.mainSection}>
-                    <Meetups />
+                    <div className={styles.allGroups}>
+                        {busy && !allMeetups ? "Loading..." : meetup}
+                    </div>
                 </div>
             </div>
         </div>
